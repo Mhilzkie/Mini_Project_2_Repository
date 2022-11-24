c4a0bd9fb53392e7720a215fd00288f17e98c320
RLDT <mhilzkie06@gmail.com>
2022-11-24 15:35:14 +0800

    Created some Files

---
+++ src/Components/Content.js
@@ -0,0 +1,10 @@
+const Content = () => {
+    return (
+        <div>
+            <h1>Content</h1>
+            <p>Content is the Body of the Page</p>
+        </div>
+    );
+};
+
+export default Content;
---
+++ src/Pages/Homepage.js
@@ -0,0 +1,17 @@
+
+import Content from "../Components/Content";
+import Header from "../Components/Header";
+import Sidenav from "../Components/Sidenav";
+
+
+function Homepage() {
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+      <Sidenav/>
+    </div>
+  );
+}
+
+export default Homepage;
